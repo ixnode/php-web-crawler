54942a56da9a849fae7a3dc9566d8e7f8e33eee2
Björn Hempel <bjoern@hempel.li>
2024-02-25 05:37:19 +0100

    Add ToLower and ToUpper functions

---
+++ src/Converter/ToUpper.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * This file is part of the ixnode/php-web-crawler project.
+ *
+ * (c) Björn Hempel <https://www.hempel.li/>
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Ixnode\PhpWebCrawler\Converter;
+
+use Ixnode\PhpWebCrawler\Converter\Base\BaseConverter;
+
+/**
+ * Class ToUpper
+ *
+ * @author Björn Hempel <bjoern@hempel.li>
+ * @version 0.1.0 (2024-02-25)
+ * @since 0.1.0 (2024-02-25) First version.
+ */
+class ToUpper extends BaseConverter
+{
+    /**
+     * Returns the converted value.
+     *
+     * @inheritdoc
+     */
+    public function getValue(bool|float|int|string|null $value): string|null
+    {
+        if (is_null($value)) {
+            return null;
+        }
+
+        return strtoupper((string) $value);
+    }
+}
+
